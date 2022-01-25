20b48386860deb199afbb3fce30b7eb88765034c
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2022-01-25 19:32:08 -0300

    prompt prints user and shell name

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:27:14 by lrocigno          #+#    #+#             */
-/*   Updated: 2022/01/24 16:21:24 by lrocigno         ###   ########.fr       */
+/*   Updated: 2022/01/25 19:31:22 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,45 @@ static void	validate_command(char *command)
 {
 	if (*command)
 		printf("minishell: %s: command not found\n", command);
-	else
-		write(1, "\n", 1);
 }
 
-int	main(void)
+static char	*gen_prompt(char **envp)
 {
+	size_t	i;
+	char	*user;
+	char	*prompt;
+
+	i = 0;
+	while (ft_strncmp("USER", envp[i], 4))
+		++i;
+	user = ft_strjoin("\e[1;32m", ft_strchr(envp[i], '=') + 1);
+	prompt = ft_strjoin(user, "@minishell\e[0m:\e[0;34m~\e[0m$: ");
+	free(user);
+	user = NULL;
+	return (prompt);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*prompt;
 	char	*input;
 
-	printf("\e[1;32mminishell\e[0m:\e[0;34m~\e[0m$: ");
-	input = readline(NULL);
+	if (argc > 1)
+	{
+		printf("minishell: %s: No such file or directory\n", argv[1]);
+		exit(127);
+	}
+	prompt = gen_prompt(envp);
+	input = readline(prompt);
 	while (ft_strncmp("exit", input, ft_strlen(input)) || !*input)
 	{
 		validate_command(input);
-		printf("\e[1;32mminishell\e[0m:\e[0;34m~\e[0m$: ");
 		free(input);
-		input = readline(NULL);
+		input = readline(prompt);
 	}
 	free(input);
+	free(prompt);
 	input = NULL;
+	prompt = NULL;
 	return (0);
 }
