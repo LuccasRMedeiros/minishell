e9d3e1a74122f2b657c22916f6b2ca11605ca4ff
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2022-01-28 22:12:13 -0300

    fixed program stopping with null input

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 20:12:16 by vgoncalv          #+#    #+#             */
-/*   Updated: 2022/01/27 21:18:18 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2022/01/28 22:11:26 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	init(t_shell *sh)
 	char	*input;
 
 	input = prompt(sh);
-	while (ft_strncmp("exit", input, ft_strlen(input)))
+	while (ft_strncmp("exit", input, ft_strlen(input)) || input == NULL)
 	{
 		if (input != NULL)
 			free(input);
