9a6053f5ef42e3a97267ef45214e3455077a4d6c
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2022-01-31 10:10:54 -0300

    feat: better interface

--- src/interface.c
+++ src/interface.c
@@ -1,35 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
+/*   interface.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 20:12:16 by vgoncalv          #+#    #+#             */
-/*   Updated: 2022/01/28 23:45:21 by lrocigno         ###   ########.fr       */
+/*   Updated: 2022/01/31 09:41:49 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-#include <readline/history.h>
+#include <prompt/prompt.h>
 
-static void	validate_input(char **input)
+static char	validate(char *input)
 {
-	if (**input)
-	{
-		add_history(*input);
-		safe_free((void **)&(*input));
-	}
+	if (input == NULL)
+		return (0);
+	if (ft_strncmp("exit", input, ft_strlen(input)) == 0)
+		return (1);
+	return (0);
 }
 
 void	interface(char *input, t_shell *sh)
 {
 	input = prompt(sh);
-	if (!ft_strncmp("exit", input, ft_strlen(input)) && *input)
+	if (validate(input))
 	{
 		safe_free((void **)&input);
-		exit(0);
+		free_sh(sh);
+		exit(EXIT_SUCCESS);
 	}
-	validate_input(&input);
+	safe_free((void **)&input);
 	interface(input, sh);
 }
