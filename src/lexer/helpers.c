cc2b2ddfc63679d0f34e23ea5abba06063c8bb96
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2022-02-13 22:43:18 -0300

    even more improvements on quotes logic

--- src/lexer/helpers.c
+++ src/lexer/helpers.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 00:06:32 by lrocigno          #+#    #+#             */
-/*   Updated: 2022/02/13 16:29:27 by lrocigno         ###   ########.fr       */
+/*   Updated: 2022/02/13 22:26:30 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,24 @@ int	is_quote(char c)
 int	is_stop(char *input, int *close_q, int cq_sig)
 {
 	char	stop;
-
+	
+	if (is_quote(*input) && get_quote() == '\0')
+		set_quote(input);
 	stop = get_quote();
-	if (stop == '\0' || *close_q == cq_sig)
+	if (stop == '\0')
 		stop = ' ';
 	if (is_quote(stop))
 	{
-		if (*input == get_quote())
-			*close_q += 1;
+		if (*input == stop)
+			*close_q +=1;
 		if (*close_q == cq_sig)
 		{
+			clear_quote();
 			if (ft_isalnum(*(input + 1)) == 0)
 				return (1);
 		}
 	}
-	else if (*input == stop || *input == '#' || *input == '\0')
+	else if (*input == stop || *input == '\0' || *input == '#')
 		return (1);
 	return (0);
 }
