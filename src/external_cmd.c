433eb1485b30463663fefe7b77208abad1135fac
OtakuOcidental <otakuocidental@debina>
2022-03-02 12:28:25 -0300

    init builtin

--- src/external_cmd.c
+++ src/external_cmd.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 20:19:05 by lrocigno          #+#    #+#             */
-/*   Updated: 2022/03/02 08:18:16 by lrocigno         ###   ########.fr       */
+/*   Updated: 2022/03/02 12:27:41 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,9 +62,7 @@ static int	sub_process(t_token *tokens, t_shell *sh)
 	argv = gen_argv(tokens);
 	envp = gen_envp(sh->env);
 	if (!argv || !envp)
-	{
 		exit(0);
-	}
 	execve(full_name, argv, envp);
 	safe_free((void **)&argv);
 	safe_free((void **)&envp);
