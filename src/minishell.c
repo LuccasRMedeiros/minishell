fdca9834cc42839c8e00624bf9942faade50be5a
Vinícius Oliveira <vinicius@myriaddev.me>
2022-03-14 17:27:53 -0300

    Adds function to initialize the shell

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.o...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:27:14 by lrocigno          #+#    #+#             */
-/*   Updated: 2022/03/14 15:05:51 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2022/03/14 17:21:20 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,21 @@
 
 t_shell	*g_sh;
 
-int	main(void)
+/**
+ * @brief Initializes the Shell
+ */
+static void init_shell(void)
 {
 	g_sh = ft_calloc(1, sizeof(t_shell));
-	ft_bzero(g_sh, sizeof(t_shell));
 	parse_env();
-	interface(NULL);
+}
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	init_shell();
+	interactive(NULL);
 	free_sh(g_sh);
 	return (EXIT_SUCCESS);
 }
