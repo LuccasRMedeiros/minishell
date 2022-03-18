3e9eb37f5495e40efa9cd11b0e255014815a1acd
Vinícius Oliveira <vinicius@myriaddev.me>
2022-03-18 08:28:27 -0300

    Fixes heredocs behavior with quoted/unquoted delimiter

--- src/lexer/heredocs.c
+++ src/lexer/heredocs.c
@@ -6,28 +6,47 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.o...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 14:54:28 by vgoncalv          #+#    #+#             */
-/*   Updated: 2022/03/17 19:34:42 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2022/03/18 08:25:10 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <lexer/lexer.h>
 #include <readline/readline.h>
 #include <unistd.h>
 
-static const char	*get_delimiter(t_token *token, char *input, size_t *offset)
+static uint8_t  is_unquoted(const char *delimiter)
 {
-	if (token == NULL)
-		return (NULL);
-	while (is_space(input[*offset]) != 0)
-		*offset += 1;
-	return (word(input, offset));
+	size_t len;
+
+	if (delimiter[0] != '"' && delimiter[0] != '\'')
+		return (1);
+	len = ft_strlen(delimiter);
+	if (len > 1 && (delimiter[len - 1] == delimiter[0]))
+		return (0);
+	return (1);
+}
+
+static uint8_t	is_delimiter(char *input, const char *delimiter)
+{
+	size_t	input_len;
+	size_t	delimiter_len;
+
+	if (is_unquoted(delimiter) == 0)
+	{
+		input_len = ft_strlen(input);
+		delimiter_len = ft_strlen(delimiter) - 2;
+		return ((input_len == delimiter_len)
+				&& (ft_strncmp(input, delimiter + 1, delimiter_len) == 0));
+	}
+	return ((ft_strcmp(input, delimiter) == 0));
 }
 
 static const char	*build_heredoc(const char *heredoc, char *input)
 {
 	const char	*temp;
 
+	if (heredoc == NULL)
+		return (NULL);
 	temp = ft_strjoin(heredoc, input);
 	safe_free((void **)&heredoc);
 	heredoc = ft_strjoin(temp, "\n");
@@ -38,23 +57,28 @@ static const char	*build_heredoc(const char *heredoc, char *input)
 static t_token	*prompt_heredoc(t_token *token, const char *delimiter)
 {
 	char		*input;
+	char		*quote;
 	const char	*heredoc;
 
 	input = NULL;
-	heredoc = ft_strdup("");
+	quote = "'";
+	if ((is_unquoted(delimiter) != 0))
+		quote = "\"";
+	heredoc = ft_strdup(quote);
 	while (1)
 	{
 		safe_free((void **)&input);
 		input = readline("here-doc: ");
-		if (input == NULL || (ft_strcmp(input, delimiter) == 0))
+		if (input == NULL || (is_delimiter(input, delimiter) != 0))
 			break ;
 		heredoc = build_heredoc(heredoc, input);
 	}
 	safe_free((void **)&input);
-	if (heredoc == NULL)
-		return (NULL);
 	token = new_token(T_WORD, token);
-	token->value = heredoc;
+	token->value = ft_strjoin(heredoc, quote);
+	safe_free((void **)&heredoc);
+	if (token->value == NULL)
+		return (NULL);
 	return (token);
 }
 
@@ -64,7 +88,11 @@ t_token	*heredocs(t_token *token, char *input, size_t *offset)
 
 	if ((token->type == T_WORD) || (ft_strcmp(token->value, "<<") != 0))
 		return (token);
-	delimiter = get_delimiter(token, input, offset);
+	while (is_space(input[*offset]) != 0)
+		*offset += 1;
+	if (input[*offset] == '\0')
+		return (NULL);
+	delimiter = word(input, offset);
 	if (delimiter == NULL)
 		return (NULL);
 	token = prompt_heredoc(token, delimiter);
